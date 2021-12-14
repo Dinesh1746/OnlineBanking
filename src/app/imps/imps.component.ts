83bc9d92f802763f5c9cc9a300662d31583d40ac
Dinesh <dineshsai1746@gmail.com>
2021-12-14 17:51:26 +0530

    imps change

--- src/app/imps/imps.component.ts
+++ src/app/imps/imps.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, RequiredValidator } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-imps',
@@ -12,11 +14,18 @@ export class ImpsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  submit(form:any){
-    var Accountnumber=form.Accountnumber;
-    console.log(Accountnumber);
-    var ToAccountnumber=form.ToAccountnumber;
-    console.log(ToAccountnumber);
-  }
+  form=new FormGroup(
+    {
+      fac:new FormControl('', [Validators.required, Validators.minLength(4)]),
+      tac:new FormControl('',[Validators.required,  Validators.minLength(4)]),
+      amount:new FormControl('',[Validators.required]),
 
+    } 
+  );
+get f(){
+    return this.form.controls;
+  }
+  submit(){
+    console.log(this.form.value);
+  }
 }
