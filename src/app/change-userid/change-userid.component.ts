88810d8ecf2752b4362d4ae7e540815c8795f7bd
Abhishek varma <abhishekvarma941@gmail.com>
2021-12-15 12:45:17 +0530

    coding

--- src/app/change-userid/change-userid.component.ts
+++ src/app/change-userid/change-userid.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-change-userid',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeUseridComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  form=new FormGroup(
+    {
+      new:new FormControl('', [Validators.required, Validators.minLength(3)]),
+      cnew:new FormControl('',[Validators.required]),
+    }
+  );
+  
+  get f(){
+    return this.form.controls;
+  }
+  
+  submit(){
+    console.log(this.form.value);
+  }
+  onSubmit() {​​​​​​​​​  
+    this.router.navigate(['/dashboard']) 
   }
 
 }
